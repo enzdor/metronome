26543256aa9bce2c4fba32c5c2c3dee496b1cc76
enzdor <enzo01lb@gmail.com>
2022-03-27 23:30:37 -0300

    simplified scrip and gave up on gui

--- metronome.py
+++ metronome.py
@@ -1,34 +1,15 @@
-import tkinter as tk
 import time
 import pygame
 
-root = tk.Tk()
-canvas = tk.Canvas(root, height=700, width=700)
-canvas.pack()
-
-frame = tk.Frame(root)
-frame.place(relwidth=1, relheight=1)
-
-playing = 1
+bpm_input = input('BPM:')
 
 def playmetronome():
     pygame.mixer.init()
-    newLabel = tk.Label(frame, text=speed.get())
-    newLabel.pack()
-    newLabel2 = tk.Label(frame, text=time.ctime())
-    newLabel2.pack()
-    bpm =  (60 / int(speed.get())) - 0.002
-    print(bpm)
-    while playing > 0:
+    bpm =  ((60 / int(bpm_input)) - 0.002)
+    alive = True
+    while alive == True:
         pygame.mixer.Sound('./sounds/Tic.mp3').play()
         time.sleep(bpm)
-    print('done')
 
+playmetronome()
 
-play = tk.Button(frame, text='play', command=playmetronome)
-play.pack()
-
-speed = tk.Entry(frame)
-speed.pack()
-
-root.mainloop()
